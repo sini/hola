c9d3ef95631d7c5288524ea4ad2be751d06a9517
Jason Bowman <jason@json64.dev>
2026-06-23 22:00:46 -0700

    feat(parity): structural order-sensitive diff oracle + soundness tests

---
+++ ci/tests/oracle.nix
@@ -0,0 +1,133 @@
+{ hola, ... }:
+let
+  p = hola.parity;
+in
+{
+  flake.tests.oracle = {
+    equal = {
+      expr =
+        (p.diff {
+          a = {
+            x = 1;
+          };
+          b = {
+            x = 1;
+          };
+        }).identical;
+      expected = true;
+    };
+    differ-scalar = {
+      expr =
+        (p.diff {
+          a = {
+            x = 1;
+          };
+          b = {
+            x = 2;
+          };
+        }).divergences;
+      expected = [
+        {
+          path = [ "x" ];
+          aValue = 1;
+          bValue = 2;
+        }
+      ];
+    };
+    missing-key = {
+      expr =
+        (p.diff {
+          a = {
+            x = 1;
+          };
+          b = { };
+        }).divergences;
+      expected = [
+        {
+          path = [ "x" ];
+          aValue = 1;
+          bValue = "__absent";
+        }
+      ];
+    };
+    list-order = {
+      expr =
+        (p.diff {
+          a = [
+            "a"
+            "b"
+          ];
+          b = [
+            "b"
+            "a"
+          ];
+        }).identical;
+      expected = false;
+    };
+    list-length = {
+      expr =
+        (p.diff {
+          a = [ "a" ];
+          b = [
+            "a"
+            "b"
+          ];
+        }).divergences;
+      expected = [
+        {
+          path = [ "length" ];
+          aValue = 1;
+          bValue = 2;
+        }
+      ];
+    };
+    nested = {
+      expr =
+        (p.diff {
+          a = {
+            s = {
+              y = [ 1 ];
+            };
+          };
+          b = {
+            s = {
+              y = [ 2 ];
+            };
+          };
+        }).divergences;
+      expected = [
+        {
+          path = [
+            "s"
+            "y"
+            0
+          ];
+          aValue = 1;
+          bValue = 2;
+        }
+      ];
+    };
+    one-arm-throw = {
+      expr =
+        (p.diff {
+          a = {
+            x = 1;
+          };
+          b = (throw "boom");
+        }).identical;
+      expected = false;
+    };
+    locate-head = {
+      expr =
+        (p.locate {
+          a = {
+            x = 1;
+          };
+          b = {
+            x = 2;
+          };
+        }).path;
+      expected = [ "x" ];
+    };
+  };
+}
--- lib/parity.nix
+++ lib/parity.nix
@@ -1,1 +1,97 @@
-{ lib }: { }
+{ lib }:
+let
+  inherit (builtins)
+    isAttrs
+    isList
+    attrNames
+    tryEval
+    deepSeq
+    length
+    elemAt
+    head
+    ;
+
+  force = x: deepSeq x x;
+
+  diffAt =
+    path: a: b:
+    if isAttrs a && isAttrs b then
+      let
+        keys = lib.unique (attrNames a ++ attrNames b);
+      in
+      lib.concatMap (
+        k:
+        if (a ? ${k}) && (b ? ${k}) then
+          diffAt (path ++ [ k ]) a.${k} b.${k}
+        else
+          [
+            {
+              path = path ++ [ k ];
+              aValue = if a ? ${k} then a.${k} else "__absent";
+              bValue = if b ? ${k} then b.${k} else "__absent";
+            }
+          ]
+      ) keys
+    else if isList a && isList b then
+      if length a != length b then
+        [
+          {
+            path = path ++ [ "length" ];
+            aValue = length a;
+            bValue = length b;
+          }
+        ]
+      else
+        lib.concatMap (i: diffAt (path ++ [ i ]) (elemAt a i) (elemAt b i)) (lib.range 0 (length a - 1))
+    else if a == b then
+      [ ]
+    else
+      [
+        {
+          inherit path;
+          aValue = a;
+          bValue = b;
+        }
+      ];
+
+  diff =
+    { a, b }:
+    let
+      ea = tryEval (force a);
+      eb = tryEval (force b);
+    in
+    if ea.success && eb.success then
+      let
+        divs = diffAt [ ] ea.value eb.value;
+      in
+      {
+        identical = divs == [ ];
+        divergences = divs;
+      }
+    else
+      {
+        identical = false;
+        divergences = [
+          {
+            path = [ ];
+            aValue = if ea.success then ea.value else "<<throw>>";
+            bValue = if eb.success then eb.value else "<<throw>>";
+          }
+        ];
+      };
+
+  locate =
+    { a, b }:
+    let
+      d = diff { inherit a b; };
+    in
+    if d.identical then null else head d.divergences;
+in
+{
+  inherit
+    force
+    diffAt
+    diff
+    locate
+    ;
+}
